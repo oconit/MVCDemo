bdc8a79ba3808b2b2b0f3852cdf3663e9c8eec79
Ana Tinoco <oconit.study@gmail.com>
2019-08-07 21:48:56 -0300

    aplicação só na controller

---
+++ MVCDemo/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCDemo.Controllers
+{
+    public class HomeController : Controller
+    {
+        //GET: Home
+        //public ActionResult Index()
+        //{
+
+
+        //    return View();
+        //}
+        
+        public string Index(string id, string name)
+        {
+            return "The value of id = " + id + " and Name = " + name;
+        }
+
+        // Exibindo texto com a controller - Não é recomendado
+        public string Texto()
+        {
+             return "Hello From ASP.NET MVC Application";
+        }
+    }
+}
